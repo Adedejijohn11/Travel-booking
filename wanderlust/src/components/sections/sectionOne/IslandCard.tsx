066f3267a85ca906fecb47fbbe988c2e9244fa8c
Adedeji John Abidemi <adedejijohn099@gmail.com>
2025-10-12 12:33:17 -0500

    make the react pagination count display none and use p tag to display the count also update useEffect function to hold the currentImg state

--- wanderlust/src/components/sections/sectionOne/IslandCard.tsx
+++ wanderlust/src/components/sections/sectionOne/IslandCard.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import data from "@/data/data.json";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { datatypes } from "@/types/dataTypes";
 import ReactPaginate from "react-paginate";
 
@@ -12,23 +12,33 @@ import { IoIosArrowRoundForward } from "react-icons/io";
 const IslandCard = () => {
   const islandData: datatypes[] = data;
   const [pageNumber, setPageNumber] = useState(0);
+  const [currentImg, setCurrentImg] = useState<string>("");
 
   const usersPerPage = 1;
+
   const pagesVisted = pageNumber * usersPerPage;
+
   const pageCount = Math.ceil(islandData.length / usersPerPage);
 
-  const displayUses = islandData.slice(pagesVisted, pagesVisted + usersPerPage);
+  const displayUser = islandData.slice(pagesVisted, pagesVisted + usersPerPage);
 
   const ChangePage = ({ selected }: { selected: number }) => {
     setPageNumber(selected);
   };
 
+  useEffect(() => {
+    const currentItem = displayUser[0]; // since only 1 item per page
+    if (currentItem) {
+      setCurrentImg(currentItem.image[2]);
+    }
+  }, [displayUser]);
+
   return (
-    <div className=" h-full w-full flex flex-row mt-10  ">
+    <div className=" h-full w-full flex flex-row mt-10">
       {/* <div> */}
 
-      {displayUses.map((data) => (
-        <div key={data.id} className=" w-[80%] flex flex-row gap-3 ">
+      {displayUser.map((data) => (
+        <div key={data.id} className=" w-[80%] flex flex-row gap-3">
           <div className="h-[380px] w-[50%] rounded-[30px] overflow-hidden">
             <Image
               src={data.image[0]}
@@ -53,7 +63,11 @@ const IslandCard = () => {
                 className="h-full w-full object-cover"
               />
             </div>
-            <div className="absolute bottom-0 right-3">
+            <div className="absolute bottom-0 right-3 flex flex-row items-center justify-center  gap-4 ">
+              <p className=" flex justify-center text-center text-[18px]  w-full text-gray-500 ">
+                {pageNumber + 1} / {pageCount}
+              </p>
+
               <ReactPaginate
                 previousLabel={<IoIosArrowRoundBack />}
                 nextLabel={<IoIosArrowRoundForward />}
@@ -65,11 +79,28 @@ const IslandCard = () => {
                 nextLinkClassName={"nextBttn"}
                 disabledClassName={"paginationDisabled"}
                 activeClassName={"paginationActive"}
+                pageRangeDisplayed={0} // hide page numbers
+                marginPagesDisplayed={0} // hide page numbers
               />
+              {/* </div> */}
             </div>
           </div>
         </div>
       ))}
+
+      <div className="relative w-[20%] ">
+        <div className="absolute bottom-0 h-[300px] w-full  rounded-l-3xl overflow-hidden">
+          {currentImg && (
+            <Image
+              src={currentImg}
+              alt="background-img"
+              width={400}
+              height={50}
+              className="h-full w-full object-cover"
+            />
+          )}
+        </div>
+      </div>
     </div>
   );
 };
